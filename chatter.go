631c8428b0cfbe0c9221a107ef4fcc863ce0e71a
Nick Dumas <nicholas.dumas@homes.com>
2018-09-17 13:54:13 +0000

    First pass at basic Chatter functionality.
    To start with, Alpha will scold you for using inappropriate language.

--- chatter.go
+++ chatter.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"strings"
+
 	dgo "github.com/bwmarrin/discordgo"
 )
 
-func Chatter(content string, conf Conf, session *dgo.Session, message *dgo.MessageCreate) {
+var (
+	SWEARS = []string{"fuck", "shit", "dick", "pussy", "ass"}
+)
 
+// ContainsAny searchs a given string for the presence of any of the provided substrings.
+func ContainsAny(s string, terms []string) bool {
+	for _, t := range terms {
+		if strings.Contains(s, t) {
+			return true
+		}
+	}
+	return false
+}
+
+func Chatter(content string, conf Conf, session *dgo.Session, message *dgo.MessageCreate) {
+	if ContainsAny(content, SWEARS) {
+		s.ChannelMessageSend("You've got a dirty mouth, you need a spanking.")
+	}
 }
