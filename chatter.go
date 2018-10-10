1cea1ae226779edc2fe8fcbe5084670b69e474b0
Nick Dumas <nicholas.dumas@homes.com>
2018-10-10 13:40:17 +0000

    Restructuring chattr to be a little more flexible.
    By using the ResponseMapping type, you can define arbitrary numbers and
    types of auto-responses. This could come from a config file, boltdb,
    doesn't super much matter.

--- chatter.go
+++ chatter.go
@@ -1,21 +1,45 @@
 package main
 
 import (
-	"golang.org/x/time/rate"
+	"math/rand"
 	"strings"
 	"time"
+
+	"golang.org/x/time/rate"
 
 	dgo "github.com/bwmarrin/discordgo"
 )
 
+type ErrNoPromptMatch struct{}
+
+func (e ErrNoPromptMatch) Error() string {
+	return "No matching prompt text."
+}
+
+type ResponseMapping struct {
+	Terms     []string
+	Responses []string
+}
+
+func (r ResponseMapping) Reply(t string) (string, error) {
+	if containsAny(t, r.Terms) {
+		return r.Responses[rand.Intn(len(r.Responses))], nil
+	}
+	return "", ErrNoPromptMatch{}
+}
+
 var (
-	r      = rate.Every(time.Duration(time.Second * 15))
-	L      = rate.NewLimiter(r, 1)
-	SWEARS = []string{"fuck", "shit", "dick", "pussy", "ass"}
+	r = rate.Every(time.Duration(time.Second * 90))
+	L = rate.NewLimiter(r, 1)
 )
 
+var Swears ResponseMapping = ResponseMapping{
+	Terms:     []string{"fuck", "shit", "dick", "pussy", "ass", "cunt", "whore", "bitch", "cock"},
+	Responses: []string{"You need a spanking.", "You kiss your mother with that mouth?", "Excuse me, this is a *Christian server*. Thank you.", "Ooooooh, I'm telling mom on you!"},
+}
+
 // ContainsAny searchs a given string for the presence of any of the provided substrings.
-func ContainsAny(s string, terms []string) bool {
+func containsAny(s string, terms []string) bool {
 	for _, t := range terms {
 		if strings.Contains(s, t) {
 			return true
@@ -25,7 +49,13 @@ func ContainsAny(s string, terms []string) bool {
 }
 
 func Chatter(c string, conf Conf, s *dgo.Session, m *dgo.MessageCreate) {
-	if L.Allow() && ContainsAny(c, SWEARS) {
-		s.ChannelMessageSend(m.ChannelID, "You've got a dirty mouth, you need a spanking.")
+	r, err := Swears.Reply(c)
+
+	if err != nil {
+		return
+	}
+
+	if L.Allow() {
+		s.ChannelMessageSend(m.ChannelID, r)
 	}
 }
